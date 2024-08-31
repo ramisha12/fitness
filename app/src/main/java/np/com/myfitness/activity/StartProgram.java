363aab1007a500d4ab554551928a104816091832
Namrata Sapkota <Namratasapkota012@gmail.com>
2024-08-31 14:53:49 +0545

    SecondTime

--- app/src/main/java/np/com/myfitness/activity/StartProgram.java
+++ app/src/main/java/np/com/myfitness/activity/StartProgram.java
@@ -1,6 +1,7 @@
 package np.com.myfitness.activity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +13,16 @@ import np.com.myfitness.R;
 
 public class StartProgram extends AppCompatActivity {
 
+    ProgressBar progressBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_start_program);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+
     }
 }
